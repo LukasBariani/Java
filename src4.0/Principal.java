6856c3b3da4761d18926fdfbcd7d384876844fbc
Lukas Bariani <lukasbariani08@gmail.com>
2025-04-29 21:49:42 -0300

    ultimos detalhes antes da parte grafica

--- src4.0/Principal.java
+++ src4.0/Principal.java
@@ -1,5 +1,9 @@
-public class Principal {
-    public static void main(String[] args) {
-        MenuConta menuConta = new MenuConta();
-    }
-}
+try {
+        menuConta.executarMenu();
+} catch (InputMismatchException e) {
+        System.out.println("Erro de entrada: valor inválido.");
+} catch (MinhaExcecaoPersonalizada e) {
+        System.out.println("Erro específico: " + e.getMessage());
+        } catch (Exception e) {
+        System.out.println("Erro inesperado: " + e.getMessage());
+        }
